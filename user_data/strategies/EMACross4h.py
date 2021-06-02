479f6d119dcea0413510377d0e63026f6ebdccfe
Chayapon <chayapon.lee13@gmail.com>
2021-06-02 13:23:51 +0700

    Edit EMACross4h, change moving average windows

--- user_data/strategies/EMACross4h.py
+++ user_data/strategies/EMACross4h.py
@@ -9,6 +9,6 @@ class EMACross4h(EMACross):
     timeframe = '4h'
 
     # Moving Average Rolling Windows
-    ma_short_window = 20
-    ma_long_window = 60
+    ma_short_window = 10
+    ma_long_window = 65
 
